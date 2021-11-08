e236ee478890b665e41e69396aa91c328242064d
00008487 <vosiqov18.o@gmail.com>
2021-11-09 02:48:49 +0500

    Setup setting in Startup class and added connection string to AWS server

--- src/LoadAPI/Startup.cs
+++ src/LoadAPI/Startup.cs
@@ -8,15 +8,23 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace LoadAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration {get;}
+        public Startup(IConfiguration configuration)
+        {
+            Configuration=configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<LoadContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
             services.AddControllers();
             services.AddScoped<ILoadAPIRepo, MockLoadAPIRepo>();
         }
