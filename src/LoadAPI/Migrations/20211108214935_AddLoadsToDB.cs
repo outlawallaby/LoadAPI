e4f26566c66ec1545a117b7901e61bbaabc89ea0
00008487 <vosiqov18.o@gmail.com>
2021-11-09 02:59:34 +0500

    Successful connection to AWS database and migration

---
+++ src/LoadAPI/Migrations/20211108214935_AddLoadsToDB.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace LoadAPI.Migrations
+{
+    public partial class AddLoadsToDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LoadItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    LoadNumber = table.Column<string>(nullable: false),
+                    Broker = table.Column<string>(nullable: false),
+                    PickUpAddress = table.Column<string>(nullable: false),
+                    DeliveryAddress = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoadItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LoadItems");
+        }
+    }
+}
